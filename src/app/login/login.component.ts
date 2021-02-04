ccc18c4ccfe88edec122ec7542bb29161effb219
Ketan <ketankatore9@gmail.com>
2021-02-05 05:09:30 +0530

    feat : adds login api

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { ApiService } from '../services/api.service';
 
 // import { UserInfo } from '../shared/interfaces/auth.interface';
 // import { AuthService } from '../shared/services/auth.service';
@@ -16,14 +20,40 @@ export class LoginComponent implements OnInit {
   };
   hide = true
   constructor(
-    // private authService: AuthService
-    ) { }
+    private spinner: NgxSpinnerService,
+    private _snackBar: MatSnackBar,
+    private api: ApiService,
+    private router: Router) { }
+
 
   ngOnInit(): void {
   }
 
+  openSnackBar(message: string, action: string) {
+    this._snackBar.open(message, action, {
+      duration: 2000,
+    });
+  }
+  validateInputs(userInfo: any) {
+    if (!userInfo.email) { this.openSnackBar('Please enter email id.', ''); return false }
+    if (!userInfo.password) { this.openSnackBar('Please enter password.', ''); return false }
+    return true;
+  }
   login(userInfo: any) {
+    if (!this.validateInputs(userInfo)) { return; };
+    this.spinner.show();
+    this.api.logIn(userInfo).subscribe((result: any) => {
+      this.spinner.hide();
+      this.openSnackBar(result.message, '')
+      if (result.status) {
+        this.router.navigateByUrl("/signup");
+      }
+      console.log(result);
+    }, (err) => {
+      this.spinner.hide();
+      this.openSnackBar(err.message || 'Internal server error', '')
+    })
     console.log(userInfo);
-    
+
   }
 }
